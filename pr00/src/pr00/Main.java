0fc629d65df733ec0c9ec6dba3db50849b2eeafe
Emin Tekin <emint2719gmail.com>
2024-04-02 22:52:23 +0300

    Update Main.java
    update

--- pr00/src/pr00/Main.java
+++ pr00/src/pr00/Main.java
@@ -1,5 +1,57 @@
 package pr00;
 
-public class Main {
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.apache.poi.wp.usermodel.HeaderFooterType;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFHeader;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+
+public class Main {
+  
+	
+	Main() throws IOException
+	{ try {
+		XWPFDocument document1=new XWPFDocument();
+		XWPFParagraph paragraph1=document1.createParagraph();
+		
+		
+		
+		
+		
+		XWPFRun run1=paragraph1.createRun();
+		
+		
+		
+		run1.addBreak();
+		run1.addBreak();
+		run1.addBreak();
+		run1.setText("Project ROPOLIMPICS");
+		run1.setFontSize(25);
+		paragraph1.setAlignment(ParagraphAlignment.CENTER);
+		
+		
+		
+		document1.write(out);
+		out.close();
+		document1.close();
+		
+		System.out.println("document  creation succeed");
+		
+	}
+	catch(Exception e) 
+	{
+		e.printStackTrace();
+	}
+}
+
+	
+	
+	
+	
+	
 }
